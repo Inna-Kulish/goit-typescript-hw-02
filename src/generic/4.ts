82b2be6d52a7e10a7bef033ed6d7f25c75e1d30e
Inna-Kulish <norelajest@gmail.com>
2023-12-19 14:20:40 +0200

    base

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,21 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface ComponentProps {
+  // Оголосіть тут необхідні властивості для базового класу Component
 }
 
-class Page extends Component {
-  pageInfo () {
+class Component<T extends ComponentProps> {
+  constructor(public props: T) {}
+}
+
+interface PageProps extends ComponentProps {
+  title: string;
+  // Додайте інші властивості, які потрібні для класу Page
+}
+
+class Page extends Component<PageProps> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
